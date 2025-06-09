7b608d3bd6cc1e26d0a4003f61cfc8ab12435405
YeVheniia <didenko11121988@ukr.net>
2025-06-09 14:42:07 +0300

    Add language param to fetchMovies

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -3,15 +3,23 @@ import type { TMDBSearchResponse } from '../types/movie';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
 
-export const fetchMovies = async (query: string, page = 1): Promise<TMDBSearchResponse> => {
-  const response = await axios.get<TMDBSearchResponse>(`${BASE_URL}/search/movie`, {
-    params: {
-      query,
-      page,
-    },
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
-    },
-  });
+export const fetchMovies = async (
+  query: string,
+  page: number
+): Promise<TMDBSearchResponse> => {
+  const response = await axios.get<TMDBSearchResponse>(
+    `${BASE_URL}/search/movie`,
+    {
+      params: {
+        query,
+        page,
+        language: 'en-US', // <--- додано мову
+      },
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      },
+    }
+  );
+
   return response.data;
 };
